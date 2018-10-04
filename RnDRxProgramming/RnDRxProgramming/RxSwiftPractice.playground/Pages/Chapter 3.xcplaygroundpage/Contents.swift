b05bf8906a487df9e6f8928eaf29e779d1504eb1
Sulaiman Khan <sulaiman.sust@gmail.com>
2018-10-04 19:05:08 +0600

    Working on Chapter 3

--- RnDRxProgramming/RnDRxProgramming/RxSwiftPractice.playground/Pages/Chapter 3.xcplaygroundpage/Contents.swift
+++ RnDRxProgramming/RnDRxProgramming/RxSwiftPractice.playground/Pages/Chapter 3.xcplaygroundpage/Contents.swift
@@ -18,6 +18,7 @@ func example(of: String, _ handler:  () -> Void) -> Void {
 enum MyError : Error {
     case firstError, secondError, thirdError
 }
+print("-----------------PublishSubject------------------")
 
 example(of: "PublishSubject") {
     let publishSubject = PublishSubject<String>()
@@ -47,3 +48,103 @@ publishSubject.on(.next("1"))
 publishSubject.onNext("2")
 publishSubject.onError(MyError.firstError)
 publishSubject.onNext("after disposed")
+print("----------------BehaviorSubject-------------------")
+
+let behavSubject = BehaviorSubject.init(value: "Initial Value")
+let subscription1 = behavSubject.subscribe(onNext: {
+    value in
+    print("1 -> \(value)")
+}).disposed(by: disposeBag)
+behavSubject.onNext("second value")
+let subscription2 = behavSubject.subscribe(onNext: {
+    value in
+    print("2 -> \(value)")
+}).disposed(by: disposeBag)
+
+
+print("----------------ReplaySubject-------------------")
+
+example(of: "ReplaySubject") {
+
+let subject = ReplaySubject<String>.create(bufferSize: 2)
+
+subject.onNext("1")
+subject.onNext("2")
+subject.onNext("3")
+
+// 3
+subject
+    .subscribe {
+        print("1) : \($0)")
+    }
+    .disposed(by: disposeBag)
+
+subject
+    .subscribe {
+        print("2) : \($0)")
+    }
+    .disposed(by: disposeBag)
+}
+
+let repSubject = ReplaySubject<String>.create(bufferSize: 3)
+repSubject.subscribe{
+    event in
+    print(event)
+    }.disposed(by: disposeBag)
+
+
+repSubject.onNext("one")
+repSubject.onNext("two")
+repSubject.subscribe{
+    event in
+    print("2 \(event)")
+    }.disposed(by: disposeBag)
+
+
+repSubject.onNext("three")
+repSubject.onError(MyError.firstError)
+
+repSubject.onNext("four")
+
+
+print("----------------Variable-------------------")
+
+example(of: "Variable") {
+
+let variable = Variable("Initial value")
+
+
+variable.value = "New initial value"
+
+variable.asObservable()
+    .subscribe {
+        print( "1)    \($0)")
+    }
+    .disposed(by: disposeBag)
+}
+
+
+print("-----------------------------------")
+
+
+print("-----------------------------------")
+
+
+
+print("-----------------------------------")
+
+
+
+print("-----------------------------------")
+
+
+
+print("-----------------------------------")
+
+
+
+print("-----------------------------------")
+
+
+print("-----------------------------------")
+
